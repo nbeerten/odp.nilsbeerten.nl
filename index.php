71d53ef542815b8b22e860824bb4928845cc6f99
nbeerten <tutlermax@hotmail.com>
2022-03-06 16:12:07 +0100

    Updated get bot_token to OOP

--- index.php
+++ index.php
@@ -1,27 +1,32 @@
 <?
-$secretfile = file_get_contents("secret.json");
-$secret = json_decode($secretfile, true);
+class secrets {
+    protected function bot_token(){
+        $secretfile = file_get_contents("secret.json");
+        $secret = json_decode($secretfile, true);
+        return $secret["bot_token"];
+    }
+}
+
 
 $id = parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
 
-class disgd_users {
-    public function get($id) {
-        global $secret;
+class disgd extends secrets {
+    public function get_users($id) {
         $url = "https://discord.com/api/v9";
         $curl = curl_init($url);
 
         curl_setopt($curl, CURLOPT_URL, $url.'/users/'.$id);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $headers = array(
-            "Authorization: Bot ".$secret["bot_token"],
-            );
-            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+                       "Authorization: Bot ".$this->bot_token(),
+                   );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
         $resp = curl_exec($curl);
         curl_close($curl);
         return $resp;
     }
 }
-$disgd_users = new disgd_users;
-echo $disgd_users->get($id);
+$disgd = new disgd;
+echo $disgd->get_users($id);
 ?>

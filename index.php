64aefc2d401a27c77031d88d788c26f8b0750d6b
nbeerten <tutlermax@hotmail.com>
2022-03-06 16:48:08 +0100

    Added Bootstrap Reboot CSS

--- index.php
+++ index.php
@@ -54,6 +54,7 @@
 
 		<!-- Custom CSS-->
 		<link rel="stylesheet" href="custom.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap-reboot.min.css">
     </head>
     <body>
         <main>

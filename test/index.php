fc745a47ac2b6d48d52a931885f461f7f1860628
nbeerten <tutlermax@hotmail.com>
2022-03-08 17:14:18 +0100

    Testing session

--- test/index.php
+++ test/index.php
@@ -1,5 +1,5 @@
 <?php
-    //* Get url query: example.com?[$id]
     echo parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY);
     echo $_SERVER['REQUEST_URI'];
+    echo $_SERVER['QUERY_STRING'];
 ?>

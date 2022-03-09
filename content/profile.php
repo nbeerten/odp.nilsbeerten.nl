4a5b36a7877b2bb7bcef993ad5a4735da18033ab
nbeerten <tutlermax@hotmail.com>
2022-03-09 21:29:39 +0100

    Testing

--- content/profile.php
+++ content/profile.php
@@ -1,41 +1,3 @@
-<?php
-    // Import Classes
-    require_once $_SERVER['DOCUMENT_ROOT'].'/class/error.php'; // For error handling
-    require_once $_SERVER['DOCUMENT_ROOT'].'/class/filter.php'; // For general filtering
-    require_once $_SERVER['DOCUMENT_ROOT'].'/class/disgd.php'; // Discord API communication
-    require_once $_SERVER['DOCUMENT_ROOT'].'/class/BitwiseHandler.php'; // For "decoding" the user flags
-
-    //* Create instances
-    $error = new errorhandler; //* Error handling
-    $filter = new filter; //* Key and input validation
-    $disgd = new disgd; //* Discord API connection
-    $bitwisehandler = new BitwiseHandler; //* "Decoding" user flags
-    
-    try {
-        //* Get url query for and validate
-        $id = $filter->userid($_GET['id']);
-
-        //* Get the data
-        $disgd_user = $disgd->get_users($id);
-
-        //* "Decoding" user flags
-        $userflags = $bitwisehandler->get($disgd_user['public_flags']);
-
-        //* Ready to output
-        $status = 1;
-        require $_SERVER['DOCUMENT_ROOT'].'/content/profile.php';
-    } catch (Error $e) {
-        $catched_error = $error->exception($e);
-        echo $catched_error;
-
-        //* Not ready to output
-        $status = 0;
-    }
-
-    //* Check if ready to output the HTML below;
-    if($status === 5) 
-    { //* Close bracket is located at end of HTML
-?>
 <!doctype html>
 <html lang="en">
 	<head>
@@ -89,5 +51,3 @@
         </main>
     </body>
 </html>
-<? }; //* Closing the bracket, normal HTML output will only happen upon a valid status
-?>
